f60b97ebd6f80dc7f6fe9b438b5604bad21cf5a1
pos <pgousdal@users.noreply.github.com>
2026-09-18 20:00:51 +0200

    Support simavr 1.6 AVR data symbol offset

--- tools/q1_twi_datapath.c
+++ tools/q1_twi_datapath.c
@@ -66,7 +66,7 @@ int main(int argc, char **argv)
     uint32_t readback_addr = 0;
     for (uint32_t i = 0; i < fw.symbolcount; ++i) {
         if (fw.symbol[i] && !strcmp(fw.symbol[i]->symbol, "twi_readback")) {
-            readback_addr = fw.symbol[i]->addr - AVR_SEGMENT_OFFSET_DATA;
+            readback_addr = fw.symbol[i]->addr - 0x00800000UL;
             break;
         }
     }
