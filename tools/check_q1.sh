c5972e4aff0f7e7b5192e6bc0c11ae2d6aa3961e
pos <pgousdal@users.noreply.github.com>
2026-09-20 14:10:47 +0200

    Probe M6 optimization result by AVR SRAM address

--- tools/check_q1.sh
+++ tools/check_q1.sh
@@ -365,7 +365,7 @@ probe_optimization() {
         -ex "target remote :1234" \
         -ex "break optimization_ready" \
         -ex "continue" \
-        -ex "p/x (unsigned int)(*(unsigned char*)&opt_result | ((unsigned int)*((unsigned char*)&opt_result + 1) << 8))" \
+        -ex "p/x (unsigned int)(*(unsigned char*)0x108 | ((unsigned int)*(unsigned char*)0x109 << 8))" \
         -ex "quit" >"$log" 2>&1
     rc=$?
     set -e
